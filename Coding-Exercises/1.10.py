4c2ee4eea35706972f9db969727dbdab69e7a39f
James Paul Tulod <paultulod@pm.me>
2022-08-15 00:35:28 +0800

    🥳 Complete Module 1

---
+++ Coding-Exercises/1.10.py
@@ -0,0 +1,9 @@
+character_job = "pirate"
+character_ride = "ship"
+character_souvenir = "gold"
+character_pet = "parrot"
+
+print(f'There was once a {character_job} who loved adventure.')
+print(f'The {character_job} would take her {character_ride} to unknown places.')
+print(f'She brings home a lot of {character_souvenir}.')
+print(f'Then she goes home to her pet {character_pet}, Chuckles.')
