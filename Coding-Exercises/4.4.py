e4517477ccb2dc4283e36985674771111703ecf5
James Paul Tulod <paultulod@pm.me>
2022-08-16 13:01:08 +0800

    🥳 Complete Module 4

---
+++ Coding-Exercises/4.4.py
@@ -0,0 +1,10 @@
+x = 332
+y = 2031
+
+if x >= y:
+    print("x is equal to y")
+elif x == y:
+    print("x is equal to y")
+else:
+    print("x is less than y")
+
---
+++ Coding-Exercises/4.8.py
@@ -0,0 +1,13 @@
+furniture = ['table', 'chair', 'cabinet', 'desk', 'couch']
+
+for x in furniture:
+    if x == 'cabinet':
+        continue
+    print(x)
+
+i = 1
+while i < 15:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 15.")
